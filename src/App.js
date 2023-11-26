32461c36ac1948e083af2b08873555a83182d179
alishuh <alishanaseem@live.co.uk>
2023-11-26 00:30:42 +0000

    initiasl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { CodeInput, EnterButton, ConfirmationPage, invalidCodeMessage } from "./code-input";
+
+function Header () {
+        return (
+                <div>
+                        <h1 className="logo">PatientAId</h1>
+                </div>
+        );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        // Create a state variable to hold the user's code
+        const [code, setCode] = useState(Array(6).fill(null));
+        const [showConfirmationPage, setShowConfirmationPage] = useState(false);
+        const [errorMessage, setErrorMessage] = useState(null);
+
+        // Create a function to update the code state variable
+        const changeCode = (index, value) => {
+                const newCode = [...code];
+                newCode[index] = value;
+                setCode(newCode);
+        };
+
+        // Create a function to run when the user clicks the submit button
+        const submitCode = () => {
+            const patientId = code.join(""); 
+            // PUT ACTUAL VALIDATION HERE
+            if (patientId.length != 6) {
+                setErrorMessage("Invalid code entered.");
+            } else {
+                setErrorMessage(null);
+                setShowConfirmationPage(true);
+            }
+        };
+
+        // Create a function to run when the user clicks the next patient button
+        const nextPatient = () => {
+                setShowConfirmationPage(false);
+                setCode(Array(6).fill(null));
+        };
+
+        // Render the components with the state and functions we created
+        return (
+            <div>
+                <Header />
+                {showConfirmationPage ? (
+                    <>
+                        <ConfirmationPage /> <br />
+                        <button onClick={nextPatient}>Next Patient</button>
+                    </>
+                ) : (
+                    <>
+                        <CodeInput
+                            onCodeChange={changeCode}
+                            code={code} />
+                        <EnterButton onSubmit={submitCode} />
+                        {errorMessage && <p>{errorMessage}</p>}
+                    </>
+                )}
+            </div>
+        );
 }
 
 export default App;
